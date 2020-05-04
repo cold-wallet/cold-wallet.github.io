9f314757594d40233e9b9f004aa023a8e412cba4
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-04 09:53:29 +0300

    Updates

---
+++ precache-manifest.4f13cb2e33cf2a4363e8768bdeff999e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2043acda9618b8c7bde86a577b437e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0269d64fa20db6654d4",
+    "url": "/static/css/main.ebdbfa8e.chunk.css"
+  },
+  {
+    "revision": "3f1fb6446500fff6904e",
+    "url": "/static/js/2.7d3468d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d3468d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0269d64fa20db6654d4",
+    "url": "/static/js/main.25e1016e.chunk.js"
+  },
+  {
+    "revision": "fb30f4b38f0804f3117e",
+    "url": "/static/js/runtime-main.7f36eac1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
