8863c3dc8c1b39a119c413cececc4f49a2ed8ea4
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-05 11:53:33 +0300

    Better styles for assets panel

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import {ResultsWrapper} from "./components/ResultsWrapper";
 import {AssetsWrapper} from "./components/AssetsWrapper";
-import MiddleLine from "./components/MiddleLine";
 
 function App() {
     return (
         <div className="dashboard">
             <AssetsWrapper/>
-            <MiddleLine/>
             <ResultsWrapper/>
         </div>
     );
