cd9f6125be8a72e1ab563129a3f52bca6e965d0b
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-05 11:26:58 +0300

    Added middle line

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import {ResultsWrapper} from "./components/ResultsWrapper";
 import {AssetsWrapper} from "./components/AssetsWrapper";
+import MiddleLine from "./components/MiddleLine";
 
 function App() {
     return (
         <div className="dashboard">
             <AssetsWrapper/>
+            <MiddleLine/>
             <ResultsWrapper/>
         </div>
     );
---
+++ src/components/MiddleLine.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './MiddleLine.css'
+
+function MiddleLine() {
+    return (
+        <div className={"middle-line"}/>
+    );
+}
+
+export default MiddleLine;
