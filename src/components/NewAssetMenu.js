71f5a5b119f6af003051fe33adad08c367ecbfbd
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-05 17:42:05 +0300

    Implemented menu closing

--- src/components/NewAssetMenu.js
+++ src/components/NewAssetMenu.js
@@ -5,7 +5,14 @@ export function NewAssetMenu({hideMenu}) {
     return (
         <div className={"new-asset-menu-wrapper"}>
             <div className={"new-asset-menu-shadow"} onClick={hideMenu}/>
-            <div className={"new-asset-menu"}/>
+            <div className={"new-asset-menu"}>
+                <div className={"close-new-asset-menu-button-wrapper"}>
+                    <button className={"close-new-asset-menu-button"}
+                            onClick={hideMenu}
+                    >x
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
