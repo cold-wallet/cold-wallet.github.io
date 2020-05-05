982b66c9bb5deb86910f8f91662b14fe429e48c7
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-05 10:45:36 +0300

    Left panel basic styles

--- src/components/AssetValue.js
+++ src/components/AssetValue.js
@@ -5,7 +5,12 @@ export class AssetValue extends React.Component {
     render() {
         return (
             <div className={"asset-item-value"}>
-                <input className={"asset-item-value-input"} type="number" defaultValue={this.props.value}/>
+                <input
+                    className={"asset-item-value-input"}
+                    type="number"
+                    defaultValue={this.props.value}
+                    disabled={true}
+                />
             </div>
         );
     }
