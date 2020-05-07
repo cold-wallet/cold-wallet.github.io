9a475b6968510409cc4efe66912ff7ed8a814bfc
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-07 19:00:04 +0300

    Cleanup

--- src/components/AssetsGroupsWrapper.js
+++ src/components/AssetsGroupsWrapper.js
@@ -27,31 +27,27 @@ export class AssetsGroupsWrapper extends React.Component {
     }
 
     render() {
-        return (<div className={"assets-groups-wrapper"}>
-            {
-                this.state.showMenu
-                    ? <NewAssetMenu
-                        hideMenu={() => this.hideMenu()}
-                        onCurrencySelected={(currency) => this.state.onCurrencySelected(currency)}
-                        assetType={this.state.showMenu}
-                    />
-                    : null
-            }
-            {
-                [
-                    this.props.savedState.cash,
-                    this.props.savedState["non-cash"],
-                    this.props.savedState.crypto,
-                ].map((group) =>
-                    <AssetsGroup key={group.type}
-                                 spawnMenu={({onCurrencySelected}) => this.spawnMenu({
-                                     type: group.type,
-                                     onCurrencySelected: onCurrencySelected
-                                 })}
-                                 group={group}
-                    />
-                )
-            }
-        </div>);
+        return <div className={"assets-groups-wrapper"}>{[
+            this.state.showMenu
+                ? <NewAssetMenu
+                    hideMenu={() => this.hideMenu()}
+                    onCurrencySelected={(currency) => this.state.onCurrencySelected(currency)}
+                    assetType={this.state.showMenu}
+                />
+                : null,
+            [
+                this.props.savedState.cash,
+                this.props.savedState["non-cash"],
+                this.props.savedState.crypto,
+            ].map((group) =>
+                <AssetsGroup key={group.type}
+                             spawnMenu={({onCurrencySelected}) => this.spawnMenu({
+                                 type: group.type,
+                                 onCurrencySelected: onCurrencySelected
+                             })}
+                             group={group}
+                />
+            ),
+        ]}</div>
     }
 }
