befb0b37a08e818f40313284549dc7a50b5a454d
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-05-13 09:05:55 +0300

    Refactored total amount into currency transformation

--- src/components/ResultsWrapper.js
+++ src/components/ResultsWrapper.js
@@ -42,7 +42,7 @@ export default class ResultsWrapper extends React.Component {
                     analyzer.buildInnerResult(i, this.props.savedState)
                 )
             }</div>
-        </div>;
+        </div>
     }
 
     getAnalyzers() {
@@ -52,9 +52,6 @@ export default class ResultsWrapper extends React.Component {
                 const usdCurrencyCode = "USD";
                 const eurCurrencyCode = "EUR";
                 const uahCurrencyCode = "UAH";
-                let totalAmountInUSD = 0.0;
-                let totalAmountInEUR = 0.0;
-                let totalAmountInUAH = 0.0;
                 const assetGroups = [
                     data.cash,
                     data["non-cash"],
@@ -62,172 +59,74 @@ export default class ResultsWrapper extends React.Component {
 
                 ];
                 return [
-                    <div key={key + usdCurrencyCode} className={"total-amount-in-one-currency"}>
-                        All in {usdCurrencyCode} :
-                        {
-                            assetGroups.map((group, i) => (
-                                <div key={i} className={"total-amount-in-currencies--group-of-assets"}>
-                                    <div className={"total-amount-in-currencies--group-name"}>{group.type}</div>
-                                    {
-                                        group.assets.map((asset, i) => {
-                                            const amountInUSD = (group.type === "crypto")
-                                                ? this.transformCryptoCurrencyToUSD(asset)
-                                                : this.transformCurrencyToUSD(asset);
-
-                                            totalAmountInUSD += +amountInUSD;
-
-                                            return <div
-                                                key={i}
-                                                className={"total-amount-in-currencies--asset-row"}
-                                            >{asset.amount} {asset.currency} ≈ {amountInUSD} {usdCurrencyCode}</div>
-                                        })
-                                    }</div>
-                            ))
-                        }{
-                        <div className={"total-amount-in-currencies--total-amounts"}>
-                            <div className={"total-amount-in-currencies--total-in-currency"}>
-                                Total:
-                                <p>{totalAmountInUSD} {usdCurrencyCode}</p>
-                            </div>
-                        </div>
-                    }
-                    </div>,
-                    <div key={key + eurCurrencyCode} className={"total-amount-in-one-currency"}>
-                        All in {eurCurrencyCode} :
-                        {
-                            assetGroups.map((group, i) => (
-                                <div key={i} className={"total-amount-in-currencies--group-of-assets"}>
-                                    <div className={"total-amount-in-currencies--group-name"}>{group.type}</div>
-                                    {
-                                        group.assets.map((asset, i) => {
-                                            const amountInEUR = (group.type === "crypto")
-                                                ? this.transformCryptoCurrencyToEUR(asset)
-                                                : this.transformCurrencyToEUR(asset);
-
-                                            totalAmountInEUR += +amountInEUR;
-
-                                            return <div
-                                                key={i}
-                                                className={"total-amount-in-currencies--asset-row"}
-                                            >{asset.amount} {asset.currency} ≈ {amountInEUR} {eurCurrencyCode}</div>
-                                        })
-                                    }</div>
-                            ))
-                        }{
-                        <div className={"total-amount-in-currencies--total-amounts"}>
-                            <div className={"total-amount-in-currencies--total-in-currency"}>
-                                Total:
-                                <p>{totalAmountInEUR} {eurCurrencyCode}</p>
-                            </div>
-                        </div>
-                    }
-                    </div>,
-                    <div key={key + uahCurrencyCode} className={"total-amount-in-one-currency"}>
-                        All in {uahCurrencyCode} :
-                        {
-                            assetGroups.map((group, i) => (
-                                <div key={i} className={"total-amount-in-currencies--group-of-assets"}>
-                                    <div className={"total-amount-in-currencies--group-name"}>{group.type}</div>
-                                    {
-                                        group.assets.map((asset, i) => {
-                                            const amountInUAH = (group.type === "crypto")
-                                                ? this.transformCryptoCurrencyToUAH(asset)
-                                                : this.transformCurrencyToUAH(asset);
-
-                                            totalAmountInUAH += +amountInUAH;
-
-                                            return <div
-                                                key={i}
-                                                className={"total-amount-in-currencies--asset-row"}
-                                            >{asset.amount} {asset.currency} ≈ {amountInUAH} {uahCurrencyCode}</div>
-                                        })
-                                    }</div>
-                            ))
-                        }{
-                        <div className={"total-amount-in-currencies--total-amounts"}>
-                            <div className={"total-amount-in-currencies--total-in-currency"}>
-                                Total:
-                                <p>{totalAmountInUAH} {uahCurrencyCode}</p>
-                            </div>
-                        </div>
-                    }
-                    </div>
+                    this.buildCurrencyTotalResult(assetGroups, usdCurrencyCode),
+                    this.buildCurrencyTotalResult(assetGroups, eurCurrencyCode),
+                    this.buildCurrencyTotalResult(assetGroups, uahCurrencyCode),
                 ];
             }
         }]
     }
 
-    transformCurrencyToUSD({amount, currency}) {
+    buildCurrencyTotalResult(assetGroups, resultCurrencyCode) {
+        let totalAmount = 0;
+        return <div key={resultCurrencyCode} className={"total-amount-in-one-currency"}>
+            All in {resultCurrencyCode} :
+            {
+                assetGroups.map((group, i) => (
+                    <div key={i} className={"total-amount-in-currencies--group-of-assets"}>
+                        <div className={"total-amount-in-currencies--group-name"}>{group.type}</div>
+                        {
+                            group.assets.map((asset, i) => {
+                                const amount = (group.type === "crypto")
+                                    ? this.transformAssetToCryptoCurrency(asset, resultCurrencyCode)
+                                    : this.transformAssetToCurrency(asset, resultCurrencyCode);
+
+                                totalAmount += +amount;
+
+                                return <div
+                                    key={i}
+                                    className={"total-amount-in-currencies--asset-row"}
+                                >{asset.amount} {asset.currency} ≈ {amount} {resultCurrencyCode}</div>
+                            })
+                        }</div>
+                ))
+            }
+            <div className={"total-amount-in-currencies--total-amounts"}>
+                <div className={"total-amount-in-currencies--total-in-currency"}>
+                    Total:
+                    <p>{totalAmount} {resultCurrencyCode}</p>
+                </div>
+            </div>
+        </div>
+    }
+
+    transformAssetToCurrency({amount, currency}, outputCurrency) {
+        let outputCurrencyNumCode = +(currencies[outputCurrency].numCode);
         let currencyNumCode = +(currencies[currency].numCode);
 
-        if (currencyNumCode === usdNumCode) {
-            return amount;
+        if (currencyNumCode === outputCurrencyNumCode) {
+            return amount
         }
 
-        const rateUahUsd = this.state.rates.filter(r => (r.currencyCodeA === usdNumCode)
+        const amountInUah = this.transformCurrencyToUAH(amount, currencyNumCode);
+
+        if (outputCurrencyNumCode === uahNumCode) {
+            return amountInUah
+        }
+
+        const rateUahToCurrency = this.state.rates.filter(r => (r.currencyCodeA === outputCurrencyNumCode)
             && (r.currencyCodeB === uahNumCode))[0];
 
-        if (!rateUahUsd) {
+        if (!rateUahToCurrency) {
             return 0;
         }
 
-        const rateCross = rateUahUsd.rateCross || ((rateUahUsd.rateBuy + rateUahUsd.rateSell) / 2);
-
-        if (currencyNumCode === uahNumCode) {
-            return amount / rateCross;
-        }
-
-        let rateToUah = this.state.rates.filter(r => (r.currencyCodeA === currencyNumCode)
-            && (r.currencyCodeB === uahNumCode))[0];
-
-        const rateCrossToUah = rateToUah.rateCross || ((rateToUah.rateBuy + rateToUah.rateSell) / 2);
-
-        const amountInUah = amount * rateCrossToUah;
+        const rateCross = rateUahToCurrency.rateCross || ((rateUahToCurrency.rateBuy + rateUahToCurrency.rateSell) / 2);
 
         return amountInUah / rateCross;
     }
 
-    transformCryptoCurrencyToUSD({amount, currency}) {
-        return amount;
-    }
-
-    transformCurrencyToEUR({amount, currency}) {
-        let currencyNumCode = +(currencies[currency].numCode);
-
-        if (currencyNumCode === eurNumCode) {
-            return amount;
-        }
-
-        const rateUahEur = this.state.rates.filter(r => (r.currencyCodeA === eurNumCode)
-            && (r.currencyCodeB === uahNumCode))[0];
-
-        if (!rateUahEur) {
-            return 0;
-        }
-
-        const rateCross = rateUahEur.rateCross || ((rateUahEur.rateBuy + rateUahEur.rateSell) / 2);
-
-        if (currencyNumCode === uahNumCode) {
-            return amount / rateCross;
-        }
-
-        let rateToUah = this.state.rates.filter(r => (r.currencyCodeA === currencyNumCode)
-            && (r.currencyCodeB === uahNumCode))[0];
-
-        const rateCrossToUah = rateToUah.rateCross || ((rateToUah.rateBuy + rateToUah.rateSell) / 2);
-
-        const amountInUah = amount * rateCrossToUah;
-
-        return amountInUah / rateCross;
-    }
-
-    transformCryptoCurrencyToEUR({amount, currency}) {
-        return amount;
-    }
-
-    transformCurrencyToUAH({amount, currency}) {
-        let currencyNumCode = +(currencies[currency].numCode);
-
+    transformCurrencyToUAH(amount, currencyNumCode) {
         if (currencyNumCode === uahNumCode) {
             return amount;
         }
@@ -240,7 +139,7 @@ export default class ResultsWrapper extends React.Component {
         return amount * rateCross;
     }
 
-    transformCryptoCurrencyToUAH({amount, currency}) {
-        return amount;
+    transformAssetToCryptoCurrency(asset, outputCurrency) {
+        return asset.amount;
     }
 }
