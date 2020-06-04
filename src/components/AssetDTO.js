a7d121e8cdc0b285557ab8839d870f83f4c016df
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-06-04 23:42:46 +0300

    Added name and description to AssetDTO

--- src/components/AssetDTO.js
+++ src/components/AssetDTO.js
@@ -3,11 +3,25 @@ export default class AssetDTO {
     type;
     amount;
     currency;
+    name;
+    description;
 
 
-    constructor(type, amount, currency) {
+    constructor(type, amount, currency, name, description) {
         this.type = type;
         this.amount = amount;
         this.currency = currency;
+        this.name = name;
+        this.description = description;
+    }
+
+    static copy(origin) {
+        return new AssetDTO(
+            origin.type,
+            origin.amount,
+            origin.currency,
+            origin.name,
+            origin.description,
+        )
     }
 }
