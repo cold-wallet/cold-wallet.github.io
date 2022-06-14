18d854461a08ed192f5d704c06f6ea9a6f3c73f6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2022-06-14 11:10:27 +0300

    Fixed the way module is exported

--- src/integration/binance/connector/helpers/utils.js
+++ src/integration/binance/connector/helpers/utils.js
@@ -1,27 +1,27 @@
 const axios = require('axios')
 
-const removeEmptyValue = obj => {
-  if (!(obj instanceof Object)) return {}
-  Object.keys(obj).forEach(key => isEmptyValue(obj[key]) && delete obj[key])
-  return obj
+export const removeEmptyValue = obj => {
+    if (!(obj instanceof Object)) return {}
+    Object.keys(obj).forEach(key => isEmptyValue(obj[key]) && delete obj[key])
+    return obj
 }
 
-const isEmptyValue = input => {
-  /**
-   * Scope of empty value: falsy value (except for false and 0),
-   * string with white space characters only, empty object, empty array
-   */
-  return (!input && input !== false && input !== 0) ||
-    ((typeof input === 'string' || input instanceof String) && /^\s+$/.test(input)) ||
-    (input instanceof Object && !Object.keys(input).length) ||
-    (Array.isArray(input) && !input.length)
+export const isEmptyValue = input => {
+    /**
+     * Scope of empty value: falsy value (except for false and 0),
+     * string with white space characters only, empty object, empty array
+     */
+    return (!input && input !== false && input !== 0) ||
+        ((typeof input === 'string' || input instanceof String) && /^\s+$/.test(input)) ||
+        (input instanceof Object && !Object.keys(input).length) ||
+        (Array.isArray(input) && !input.length)
 }
 
-const buildQueryString = params => {
-  if (!params) return ''
-  return Object.entries(params)
-    .map(stringifyKeyValuePair)
-    .join('&')
+export const buildQueryString = params => {
+    if (!params) return ''
+    return Object.entries(params)
+        .map(stringifyKeyValuePair)
+        .join('&')
 }
 
 /**
@@ -29,42 +29,33 @@ const buildQueryString = params => {
  * E.g. symbols=["BTCUSDT","BNBBTC"] instead of symbols[]=BTCUSDT&symbols[]=BNBBTC
  */
 const stringifyKeyValuePair = ([key, value]) => {
-  const valueString = Array.isArray(value) ? `["${value.join('","')}"]` : value
-  return `${key}=${encodeURIComponent(valueString)}`
+    const valueString = Array.isArray(value) ? `["${value.join('","')}"]` : value
+    return `${key}=${encodeURIComponent(valueString)}`
 }
 
 const getRequestInstance = (config) => {
-  return axios.create({
-    ...config
-  })
+    return axios.create({
+        ...config
+    })
 }
 
-const createRequest = (config) => {
-  const { baseURL, apiKey, method, url } = config
-  return getRequestInstance({
-    baseURL,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-MBX-APIKEY': apiKey
-    }
-  }).request({
-    method,
-    url
-  })
+export const createRequest = (config) => {
+    const {baseURL, apiKey, method, url} = config
+    return getRequestInstance({
+        baseURL,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-MBX-APIKEY': apiKey
+        }
+    }).request({
+        method,
+        url
+    })
 }
 
-const flowRight = (...functions) => input => functions.reduceRight(
-  (input, fn) => fn(input),
-  input
+export const flowRight = (...functions) => input => functions.reduceRight(
+    (input, fn) => fn(input),
+    input
 )
 
-const defaultLogger = console
-
-module.exports = {
-  isEmptyValue,
-  removeEmptyValue,
-  buildQueryString,
-  createRequest,
-  flowRight,
-  defaultLogger
-}
+export const defaultLogger = console
